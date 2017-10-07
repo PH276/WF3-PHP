2ff76ff10a4236a29df0df77f1d4823319a787a0
PH276 <pascal.huitorel@lepoles.com>
2017-10-08 01:44:12 +0200

    Auto stash before merge of "master" and "origin/master"

--- site/inc/init.inc.php
+++ site/inc/init.inc.php
@@ -3,7 +3,7 @@
 session_start();
 
 // connexxion BDD
-$pdo = new PDO("mysql:host=localhost;dbname=site", 'root', '', array(
+$pdo = new PDO("mysql:host=localhost;dbname=site", 'root', '1111', array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 ));
@@ -13,7 +13,7 @@ $msg = ''; // message pour l'utilisateur
 $page = ''; // page en cours
 $contenu = '';
 // chemins
-define('RACINE_SITE', '/PHP/site/');
+define('RACINE_SITE', '/WF3-PHP/site/');
 
 // autres inclusions
 require('fonctions.inc.php');
